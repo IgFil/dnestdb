5cac2da3a6507279667a1080b68b21ce0ab63cd8
IgFil <20ignat05@list.ru>
2019-03-28 11:49:16 +0200

    1.5.2 (beta) NEWS SYSTEM

---
+++ admin/news/news.php
@@ -0,0 +1,14 @@
+<?php
+require '../../db.php';
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Управление новостями</title>
+    <link rel="stylesheet" href="/css/main.css" type="text/css">
+</head>
+<body>
+
+</body>
+
+</html>
