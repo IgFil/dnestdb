f7f68f1a3ffdcb8ba7f78b230913d14ff23a20b5
IgFil <20ignat05@list.ru>
2019-03-28 12:59:33 +0200

    1.5.3.1 (beta) NEWS SYSTEM ADD SYSTEM

--- admin/news/news_create.php
+++ admin/news/news_create.php
@@ -9,11 +9,37 @@ require '../../db.php';
 </head>
 <body>
 <h1>Создание новости</h1>
-<form action="news_create.php" method="POST">
+<div class="container">
+    <form action="news_create.php" method="POST">
 <input name="title" placeholder="Название новости">
  <textarea name="body" placeholder="Суть новости"></textarea>
     <button name="go_to_news">Отправить</button>
 </form>
+    <?php
+    if (isset($_POST['go_to_news'])) {
+        $errors = array();
+        if ($_POST['title'] == '') {
+            $errors[] = "Введите название новости";
+
+        }
+        if ($_POST['body'] == '') {
+            $errors[''] = "Введите суть новости";
+        }
+        if (empty($errors)) {
+            $news = R::dispense('news');
+            $news->title = $_POST['title'];
+            $news->body = $_POST['body'];
+            $news->time_create = date("H:i:s d/m/Y");
+            R::store($news);
+            echo '<div id="complete">Новость успешно добавленна</div>';
+        } else {
+            echo '<div id="errors">' . array_shift($errors) . '</div>';
+        }
+
+    }
+
+    ?>
+</div>
 </body>
 </html>
 
