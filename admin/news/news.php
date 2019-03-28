290187d56198d4f4eff1e1fbc37885653b358970
KATAN <dek90257@gmail.com>
2019-03-28 12:10:40 +0200

    1.5.1

--- admin/news/news.php
+++ admin/news/news.php
@@ -8,7 +8,14 @@ require '../../db.php';
     <link rel="stylesheet" href="/css/main.css" type="text/css">
 </head>
 <body>
-
+<header>
+<nav class="top-menu">
+    <ul class="menu-main">
+      <li class="left-item"><a href="news_create.php">Создать новость</a></li>
+        <li class="left-item"><a href=news_edit.php">Редактировать</a></li>
+    </ul>
+</nav>
+</header>
 </body>
 
 </html>
