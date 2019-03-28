046c954a2b2dfabaa458d9ed262ae0d10dc0ac71
KATAN <dek90257@gmail.com>
2019-03-28 12:33:46 +0200

    1.5.4 create form in news_create.php

--- admin/news/news_create.php
+++ admin/news/news_create.php
@@ -1,3 +1,19 @@
 <?php
 require '../../db.php';
 ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Создать новость</title>
+    <link rel="stylesheet" href="/css/main.css" type="text/css">
+</head>
+<body>
+<h1>Создание новости</h1>
+<form action="news_create.php" method="POST">
+<input name="title" placeholder="Название новости">
+ <textarea name="body" placeholder="Суть новости"></textarea>
+    <button name="go_to_news">Отправить</button>
+</form>
+</body>
+</html>
+
